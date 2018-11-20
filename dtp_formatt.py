6be2f6efa4ad3e93616bc47f5ccde55d2427df83
Matthew Allcock <mallcock1@sheffield.ac.uk>
2018-11-20 09:55:31 +0000

    another fibril added

--- dtp_formatt.py
+++ dtp_formatt.py
@@ -458,10 +458,21 @@ createmapcube(roi_x=[0,100], roi_y=[0,100], getmaps = True, submap = False)
 #slit_coords_x = [172, 172]
 #slit_coords_y = [450, 499]
 
-slit_coords_x = [611, 580]
-slit_coords_y = [282, 371]
+#fibril2
+#slit_coords_x = [591, 580]
+#slit_coords_y = [265, 371]
 
-def distancetime(getintens = False, xfinal=slit_coords_x[0], xinitial=slit_coords_x[1], yfinal=slit_coords_y[0], yinitial=slit_coords_x[1]):
+#fibril 1 (fig 4 morton)
+slit_coords_x = [425, 429]
+slit_coords_y = [646, 546]
+
+#fibril 3
+slit_coords_x = [775, 775]
+slit_coords_y = [430, 490]
+
+def distancetime(xfinal=slit_coords_x[0], xinitial=slit_coords_x[1], 
+                 yfinal=slit_coords_y[0], yinitial=slit_coords_y[1],
+                 getintens=False, get_slice=False, frame_to_slice=0):
     
     """
     DISTAnces in pixels
@@ -503,23 +514,23 @@ def distancetime(getintens = False, xfinal=slit_coords_x[0], xinitial=slit_coord
         intensity1 = []
 
         
-        
+        global num
         num = np.sqrt((xfinal - xinitial)**2 + (yfinal - yinitial)**2) #aia resolution relative. Calculated by doing space.max()/0.435
-        
+        print("num = " + str(num))
         global x,y
-        x,y = np.linspace(xfinal,xinitial,num), np.linspace(yfinal,yinitial,num) #changed the order of xp1,xp0 and yp1,yp0
+        x,y = np.linspace(xfinal, xinitial, num), np.linspace(yfinal, yinitial, num) #changed the order of xp1,xp0 and yp1,yp0
 
           
         for i in range(len(total_maps)):
     
-            intensity1.append(scipy.ndimage.map_coordinates(np.transpose(total_maps[i].data),np.vstack((x,y))))
+            intensity1.append(scipy.ndimage.map_coordinates(np.transpose(total_maps[i].data), np.vstack((x,y))))
             
         cad = 7.68
         global time
         time = np.linspace(0, len(intensity1)*cad, len(intensity1))
               
         global space
-        space = np.linspace(0, np.sqrt(((xfinal-xinitial)**2 + (yfinal-yinitial)**2))*50., len(intensity1[0])) # multiply by 0.428 instead to convert into Mm
+        space = np.linspace(0, num*50., len(intensity1[0])) # multiply by 0.428 instead to convert into Mm
         
         intensity1=np.array(intensity1)
 
@@ -534,24 +545,94 @@ def distancetime(getintens = False, xfinal=slit_coords_x[0], xinitial=slit_coord
  
     
 import numpy as np
-#time_window_pix = np.array([100,140]) #in pixels
-time_window_pix = np.array([0,238]) #in pixels
-time_window = time_window_pix * 7.68 # in s
-time_frames = time_window_pix[1] - time_window_pix[0]
+#time_window_frames = np.array([100,140]) #in frames 
+time_window_frames = np.array([132,175]) #in frames
+#time_window_frames = np.array([0,71]) #in frames (fibril2)
+#time_window_frames = np.array([119,220]) #in frames (fibril1 (fig 4 morton))
+
+time_window = time_window_frames * 7.68 # in s
+number_of_frames = time_window_frames[1] - time_window_frames[0]
     
 #       
 distancetime(getintens=True)
 #
-plt.imshow(intensity1.T[:,time_window_pix[0]:time_window_pix[1]], aspect='auto', extent=[time_window[0], time_window[1], space.min(), space.max()], interpolation=None, origin='lower') #1100, 1500
+
+print("\nSlicing intensity....")
+intensity_slice = -intensity1[181]
+plt.figure()
+plt.plot(intensity_slice)
+
+time_vals = np.linspace(time_window_frames[0], time_window_frames[1], number_of_frames + 1)
+x_vals = np.arange(num) #num - 1
+
+#fit a Gaussian curve
+from scipy.optimize import curve_fit
+def gauss_func(x, A, x0, sigma, offset): #a = amplitude, b = angular frequency
+    return np.abs(A) * np.exp(-(x - x0)**2 / (2*sigma**2)) + offset
+
+boundary_t_vals = []
+boundary_x_vals_t = []
+boundary_x_vals_b = []
+#
+for t in np.linspace(time_window_frames[0], time_window_frames[1] - 1, number_of_frames):
+    t = int(t)
+#    print('t = ' + str(t))
+    params, params_covariance = curve_fit(gauss_func, x_vals, -intensity1[t], p0=[0.5, 45., 10., -1.1])
+#    print('params =' + str(params))
+    boundary_t_vals.append(t * 7.68)
+    boundary_x_vals_t.append((params[1] + np.sqrt(2*np.log(2))*params[2]) * 50)
+    boundary_x_vals_b.append((params[1] - np.sqrt(2*np.log(2))*params[2]) * 50)
+
+
+
+
+
+
+
+
+
+
+
+## Plot the data with the best-fit model
+#params, params_covariance = curve_fit(gauss_func, x_vals, intensity_slice, p0=[0.5, 45., 10., -1.1])
+#plt.figure()
+#plt.plot(x_vals, intensity_slice, '-')
+#plt.plot(x_vals, gauss_func(x_vals, params[0], params[1], params[2], params[3]), '-', color='red')
+#plt.plot([params[1] - np.sqrt(2*np.log(2))*params[2], params[1] + np.sqrt(2*np.log(2))*params[2]],
+#         [params[3] + 0.5*params[0], params[3] + 0.5*params[0]], 'ro')
+
+
+
+
+
+
+
+
+
+
+
+
 
 plt.figure()
-plt.imshow(total_maps[0].data, aspect='auto', interpolation=None, origin='lower')
-plt.plot(slit_coords_x, slit_coords_y, color='white')
+plt.imshow(intensity1.T[:,time_window_frames[0]:time_window_frames[1]], 
+           aspect='auto', interpolation=None, origin='lower', extent=[time_window[0], time_window[1], space.min(), space.max()]) #1100, 1500 #To change x/y lims: add kwarg extent=[time_window[0], time_window[1], space.min(), space.max()]
+plt.xlabel('Time (s)')
+plt.ylabel('Distance (km)')
+plt.plot(boundary_t_vals, boundary_x_vals_b, 'wo')
+plt.plot(boundary_t_vals, boundary_x_vals_t, 'wo')
+plt.ylim([space.min(), space.max()])
+plt.savefig('fibril3_dt.png')
+
+######################################
+
+#plt.figure()
+#plt.imshow(total_maps[0].data, aspect='auto', interpolation=None, origin='lower')
+#plt.plot(slit_coords_x, slit_coords_y, color='white')
 
 
 
 
-#################################
+##################################
 #Animate through time
 import matplotlib.animation as animation
 
@@ -562,8 +643,9 @@ def updatefig(i):
     im.set_array(image_data)
     return im
 
-im = plt.imshow(total_maps[time_window_pix[0]].data, aspect='auto', interpolation='gaussian', animated=True, origin='lower')
+im = plt.imshow(total_maps[time_window_frames[0]].data, aspect='auto', 
+                interpolation=None, animated=True, origin='lower')
 
-ani = animation.FuncAnimation(fig, updatefig, frames=time_frames, interval=200, repeat_delay = 1000) #interval = delay between each fram in ms
+ani = animation.FuncAnimation(fig, updatefig, frames=number_of_frames, interval=200, repeat_delay = 1000) #interval = delay between each fram in ms
 plt.plot(slit_coords_x, slit_coords_y, color='white')
 

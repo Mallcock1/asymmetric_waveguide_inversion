baeb45d50c6b4ee06359f8b064941c2f2ce7bd95
Matthew Allcock <mallcock1@sheffield.ac.uk>
2018-11-27 17:48:21 +0000

    further OOP edits

--- time-distance.py
+++ time-distance.py
@@ -16,6 +16,7 @@ import astropy.units as u
 from skimage import exposure
 import numpy as np
 import scipy as sc
+from scipy.optimize import curve_fit
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -32,7 +33,7 @@ class Full_map:
 
     if os.path.exists(file_path):
         print("\nDirectory exists.. ")
-        #checking
+        # checking
         counter = 0
         for file in total_files:
             if file.endswith("s"):
@@ -41,11 +42,10 @@ class Full_map:
     if len(total_files) == counter:
         print("\nTotal number of FITS files in this directory is %s. \n" % len(total_files))
 
-
     def createmapcube(self):
         """
         This function creates the data/map cube.
-    
+
         Inputs:
             getmaps- whether you want to create the mapcube in the given directory.
             plot- creates the standard plot of the first image.
@@ -54,48 +54,43 @@ class Full_map:
             draw_limb- plots only the limb.
             submap - whether you want to look at a specific region of interest.
             vmin,vmax (optional) - intensity of images.
-    
+
         Example call:
             >> [in]: from dtp import createmapcube
             >> [in]: createmapcube(getmaps=True)
         """
         print("\nCreating datacube... ".format(len(total_files)))
         return sunpy.map.Map(str(self.file_path) + '/*ts', sequence=True)
-    
+
         if len(total_maps) == len(total_files):
             print("\nMapcube created.")
         else:
             raise Exception("The number of files does not equal the number of "
                             "maps. Please create mapcube again.")
 
-
-    total_maps = createmapcube(file_path)
-
-    def distancetime(self, xfinal, xinitial, yfinal, yinitial):
+    def distancetime(self, slit_coords):
         """
         Distances in pixels
 
-        This function creates the distance-time plots along the slice coordiantes.
+        Creates the distance-time plots along the slice coordiantes.
 
         Inputs:
-        getintens- whether to calculate the intensity values along the slice.
-        dt- whether you want to plot only distance time plot.
-        peaks, threhsold, box_size- finds the maximum intensity value on distance 
-                                    time plots. (For time series analysis).
-        intensityslice - I = I(t,s(x,y)).
+        slit_coords = [xinit, xfinal, yinit, yfinal]
         """
+
+        total_maps = createmapcube(self.file_path)
 
         print("\nCalculating the intensity values along slit....")
 
         intensity1 = []
 
         # Number of points we can interpolate along the slit.
-        num = np.sqrt((xfinal - xinitial)**2 + (yfinal - yinitial)**2)
+        num = np.sqrt((slit_coords[1] - slit_coords[0])**2 + (slit_coords[3] - slit_coords[2])**2)
         print("num = " + str(num))
-        
+
         global x, y
-        x = np.linspace(xfinal, xinitial, num)
-        y = np.linspace(yfinal, yinitial, num)
+        x = np.linspace(slit_coords[0], slit_coords[1], num)
+        y = np.linspace(slit_coords[2], slit_coords[3], num)
     
         for i, m in enumerate(total_maps):
             intensity1.append(sc.ndimage.map_coordinates(np.transpose(m.data),
@@ -108,72 +103,77 @@ class Full_map:
         space = np.linspace(0, num*50., len(intensity1[0]))  # multiply by 50 to convert into km
 
         return np.array(intensity1)
-    
-    
-    
-    ######### EDIT FROM HERE
-    
 
-    def intensity_slice(self):
-        print("\nSlicing intensity....")
-        intensity1 = distancetime(xfinal=slit_coords_x[0], xinitial=slit_coords_x[1],
-                                  yfinal=slit_coords_y[0], yinitial=slit_coords_y[1])
-        intensity_slice = -intensity1[181]
-        plt.figure()
-        plt.plot(intensity_slice)
+    def intensity_slice(self, slit_coords, time_frames):
+        """
+        Create intensity slices for t in time_frames
 
-    time_window = time_window_frames * 7.68  # in s
-    number_of_frames = time_window_frames[1] - time_window_frames[0]
-    
+        Inputs:
+            slit_coords = [xinit, xfinal, yinit, yfinal]
+            time_frames = [t1, t2, ...]
+        """
+        for tf in time_frames:
+            print("\nSlicing intensity at frame number " + str(tf))
+            intensity1 = self.distancetime(slit_coords=slit_coods)
+            intensity_slice = -intensity1[tf]
+            plt.figure()
+            plt.plot(intensity_slice)
+
+    def find_boundaries(self, slit_coords, time_range, plot=False,
+                        savefig=None):
+        """
+        Find boundaries of the structure using gauss fitting. The edges are the
+        half-maximum points on each side of the structure.
+        
+        Inputs:
+            slit_coords = [xinit, xfinal, yinit, yfinal]
+            time_range = [tinit, tfinal]
+        """
+        time_range_s = time_range * 7.68  # in s
+        number_of_frames = time_range[1] - time_range[0]
+        time_vals = np.linspace(time_range[0], time_range[1],
+                                number_of_frames + 1)
+
+        intensity1 = self.distancetime(slit_coords=slit_coords)
+        # s is the coordinate along the slit
+        s_vals = np.arange(len(intensity1))
+
+        # fit a Gaussian curve
+
+        def gauss_func(x, A, x0, sigma, offset):
+            return np.abs(A) * np.exp(-(x - x0)**2 / (2*sigma**2)) + offset
+
+        boundary_t_vals = []
+        boundary_x_vals_t = []
+        boundary_x_vals_b = []
+
+        for i,t in enumerate(time_vals):  # np.linspace(time_frames[0], time_frames[1] - 1, number_of_frames):
+            t = int(t)
+            params, params_covariance = curve_fit(gauss_func, s_vals,
+                                                  -intensity1[t],
+                                                  p0=[0.5, 45., 10., -1.1])
+            boundary_t_vals.append(t * 7.68)
+            boundary_x_vals_t.append((params[1] + np.sqrt(2*np.log(2))*params[2]) * 50)
+            boundary_x_vals_b.append((params[1] - np.sqrt(2*np.log(2))*params[2]) * 50)
+
+        if plot is True:
+            plt.figure()
+            plt.imshow(intensity1.T[:, time_range[0]:time_range[1]],
+                       aspect='auto', interpolation=None, origin='lower',
+                       extent=[time_range_s[0], time_range_s[1],
+                               space.min(), space.max()])  # 1100, 1500 #To change x/y lims: add kwarg extent=[time_window[0], time_window[1], space.min(), space.max()]
+            plt.xlabel('Time (s)')
+            plt.ylabel('Distance (km)')
+            plt.plot(boundary_t_vals, boundary_x_vals_b, 'wo')
+            plt.plot(boundary_t_vals, boundary_x_vals_t, 'wo')
+            plt.ylim([space.min(), space.max()])
+            if savefig is not None:
+                plt.savefig(savefig)
 
 
 
 
-
-
-time_vals = np.linspace(time_window_frames[0], time_window_frames[1],
-                        number_of_frames + 1)
-# s is the coordinate along the slit
-s_vals = np.arange(len(intensity1))  # num - 1
-
-#fit a Gaussian curve
-from scipy.optimize import curve_fit
-def gauss_func(x, A, x0, sigma, offset): #a = amplitude, b = angular frequency
-    return np.abs(A) * np.exp(-(x - x0)**2 / (2*sigma**2)) + offset
-
-boundary_t_vals = []
-boundary_x_vals_t = []
-boundary_x_vals_b = []
-#
-for t in np.linspace(time_window_frames[0], time_window_frames[1] - 1, number_of_frames):
-    t = int(t)
-#    print('t = ' + str(t))
-    params, params_covariance = curve_fit(gauss_func, s_vals, -intensity1[t], p0=[0.5, 45., 10., -1.1])
-#    print('params =' + str(params))
-    boundary_t_vals.append(t * 7.68)
-    boundary_x_vals_t.append((params[1] + np.sqrt(2*np.log(2))*params[2]) * 50)
-    boundary_x_vals_b.append((params[1] - np.sqrt(2*np.log(2))*params[2]) * 50)
-
-
-    def td_gauss_plot(self.savefig=None):
-        intensity1 = self.distancetime(xfinal=slit_coords_x[0],
-                                       xinitial=slit_coords_x[1],
-                                       yfinal=slit_coords_y[0],
-                                       yinitial=slit_coords_y[1])
-        plt.figure()
-        plt.imshow(intensity1.T[:, time_window_frames[0]:time_window_frames[1]],
-                   aspect='auto', interpolation=None, origin='lower',
-                   extent=[time_window[0], time_window[1],
-                           space.min(), space.max()])  # 1100, 1500 #To change x/y lims: add kwarg extent=[time_window[0], time_window[1], space.min(), space.max()]
-        plt.xlabel('Time (s)')
-        plt.ylabel('Distance (km)')
-        plt.plot(boundary_t_vals, boundary_x_vals_b, 'wo')
-        plt.plot(boundary_t_vals, boundary_x_vals_t, 'wo')
-        plt.ylim([space.min(), space.max()])
-        if savefig is not None:
-            plt.savefig(savefig)
-
-
+#EDIT FROM HERE
 
 
 
